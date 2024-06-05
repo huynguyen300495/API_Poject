27ee7e2b0d58b63ba1556d5723c3475e7035a5a5
HuyNguyen <huynguyen300495@gmail.com>
2024-06-05 13:47:01 -0700

    Initial Commit

---
+++ API_IOS/API_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_IOSApp.swift
+//  API_IOS
+//
+//  Created by Huy Nguyen on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct API_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
